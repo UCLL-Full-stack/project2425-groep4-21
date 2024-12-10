2f064352569b86c2c67649e864bd532a0c9a1485
Andre <r0931885@ucll.be>
2024-12-10 01:09:43 +0100

    swr media

--- front-end/pages/beoordeling/index.tsx
+++ front-end/pages/beoordeling/index.tsx
@@ -2,21 +2,19 @@ import Head from 'next/head';
 import Header from '@components/header';
 import BeoordelingOverviewTable from '@components/beoordeling/BeoordelingOverviewTable';
 import { Beoordeling } from '@types';
-import { useState, useEffect } from 'react';
+import useSWR from 'swr';
 import BeoordelingService from '@services/BeoordelingService';
 
+const fetcher = async () => {
+    const response = await BeoordelingService.getAllBeoordelingen();
+    return response.json();
+};
+
 const BeoordelingPage: React.FC = () => {
-    const [beoordelingen, setBeoordelingen] = useState<Array<Beoordeling>>([]); // props die we declareren
+    const { data: beoordelingen, error } = useSWR<Array<Beoordeling>>('/api/beoordelingen', fetcher);
 
-    const getBeoordelingen = async () => {
-        const response = await BeoordelingService.getAllBeoordelingen();
-        const beoordelingData = await response.json();
-        setBeoordelingen(beoordelingData);
-    };
-
-    useEffect(() => {
-        getBeoordelingen();
-    }, []);
+    if (error) return <div>Failed to load</div>;
+    if (!beoordelingen) return <div>Loading...</div>;
 
     return (
         <>
@@ -25,10 +23,9 @@ const BeoordelingPage: React.FC = () => {
             </Head>
             <Header />
             <main className="d-flex flex-column justify-content-center align-items-center">
-                <h1>Beoordelingen</h1>
                 <section>
                     <h2>Beoordeling overzicht</h2>
-                    {beoordelingen && <BeoordelingOverviewTable beoordelingen={beoordelingen} />}
+                    <BeoordelingOverviewTable beoordelingen={beoordelingen} />
                 </section>
             </main>
         </>
